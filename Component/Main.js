8813ea2a0e20f831d9efb9f7b72c4f7e8364b4d5
zhuxf <18601345744@163.com>
2016-08-04 16:30:55 +0800

    Navigatior MainView

---
+++ Component/Main.js
@@ -0,0 +1,38 @@
+/* jshint esversion: 6 */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Welcome to React Native!
+        </Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
+
+module.exports = Main;
