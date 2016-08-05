d74d2baa4b51c3602f558a9cf34d3b5943f1fee9
zhuxf <18601345744@163.com>
2016-08-05 15:45:52 +0800

    add ListView

--- app/Mine.js
+++ app/Mine.js
@@ -9,20 +9,68 @@
    AppRegistry,
    StyleSheet,
    Text,
-   View
+   View,
+   ListView
  } from 'react-native';
 
+import InfoData from '../LocalData/MineInfos.json';
+
 class Mine extends Component {
+  constructor(props) {
+    super(props);
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(InfoData.square_list)
+    };
+
+    this.renderRow = this.renderRow.bind(this);
+    this.getInfosFromApiAsync = this.getInfosFromApiAsync.bind(this);
+  }
+
+  componentDidMount(){
+      this.getInfosFromApiAsync();
+  }
+
+  renderRow(rowData) {
+    return (
+      <Text
+
+      >
+        {rowData.name}
+      </Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          我的
-        </Text>
+
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
 
       </View>
     );
   }
+
+  getInfosFromApiAsync() {
+    fetch('http://api.budejie.com/api/api_open.php?a=square&c=topic')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("=========>", responseJson.square_list);
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          dataSource: ds.cloneWithRows(responseJson.square_list),
+        });
+      })
+      .catch((error) => {
+        console.log("=========>", error);
+      });
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -30,7 +78,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
+    marginTop: 25
   },
 
 });
